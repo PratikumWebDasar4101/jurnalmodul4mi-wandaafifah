dbdd15496dcd907733bddea14ef8cb012a40dd37
wandaafifah <42725331+wandaafifah@users.noreply.github.com>
2018-09-24 10:58:57 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,19 @@
 <?php
+$Hobi = $_POST['Hobi'];
+echo "Hobi kesukaan :";
+foreach ($Hobi as $key => $value) {
+	# code...
+}
 
+{
+	print_r($_POST['Hobi']);
+}
+$file = $_FILES['dokumen'];
+$nama_file = $file['name'];
+$nama_tmp = $file['tmp_name'];
+$upload_dir ="upload/";
+move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
+echo "uploaded.<br>";
 ?>
+<img src="upload/<?php echo $nama_file; ?>"><br><br>
+<a href="indexx.html"> Halaman awal </a>
